3da3fe8588224ef649ecf4014b502d2da04feea9
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-05-05 21:17:22 -0300

    API v. 1

--- projetos/api/routes/api.php
+++ projetos/api/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DogsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/' , [DogsController::class,'index']);
+Route::resource('dogs', DogsController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
